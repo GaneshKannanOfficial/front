be09dc2cb45926755a9ac703ed1688d106d34c0f
ganeshackzz <kannanganesh2003@gmail.com>
2024-03-15 15:16:24 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import AddStudentPage from './components/AddStudentPage';
+import ViewStudents from './components/ViewStudents';
+import AddMarks from './components/AddMarks';
+import GenerateReport from './components/GenerateReport';
+import Analysis from './components/Analysis'; // Import the Analysis componen
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/add-student" />} />
+        <Route path="/add-student" element={<AddStudentPage />} />
+        <Route path="/view-students" element={<ViewStudents />} />
+        <Route path="/add-marks" element={<AddMarks />} />
+        <Route path="/generate-report" element={<GenerateReport />} />
+        <Route path="/analysis" element={<Analysis />} /> {/* Add the route for Analysis */}
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
